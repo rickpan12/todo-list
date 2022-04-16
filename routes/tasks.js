2f4bfb8532eee0af8c7d7fcb173580b44c28b849
Rick Pan <rick.pan@uwaterloo.ca>
2022-04-15 22:38:00 -0400

    Final touches

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+//get functions from controllers file
 const {getAllTasks,createTask, getTask, updateTask, deleteTask} = require('../controllers/tasks.js')
 
+//chain HTTP requests 
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
